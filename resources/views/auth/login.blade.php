f41d9922969b994254d9dabd3489719ca3a7cee8
ruthhhs <ruthseptrianas@gmail.com>
2026-09-11 18:54:27 +0700

    feat: style login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
+    <form method="POST" action="{{ route('login') }}" class="pt-2 pb-5">
         @csrf
 
         <!-- Email Address -->
@@ -25,23 +25,27 @@
         </div>
 
         <!-- Remember Me -->
-        <div class="block mt-4">
+        <!-- <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
                 <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
             </label>
-        </div>
+        </div> -->
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
+        <div class="flex flex-col items-center justify-center mt-4">
+            <!-- @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
-            @endif
+            @endif -->
 
-            <x-primary-button class="ms-3">
+            <x-primary-button class="ms-3 mt-8">
                 {{ __('Log in') }}
             </x-primary-button>
+
+            <x-secondary-button class="ms-3 mt-5">
+                {{ __('Continue as Guest') }}
+            </x-secondary-button>
         </div>
     </form>
 </x-guest-layout>
