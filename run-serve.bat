c467c6a7adc7bc184d33559e2cb40f9595cfcb11
ruthhhs <ruthseptrianas@gmail.com>
2026-09-14 16:13:32 +0700

    update run-serve.bat

--- run-serve.bat
+++ run-serve.bat
@@ -1,4 +1,7 @@
 @echo off
 
+echo === Menginstall module frontend...
+call npm install
+
 echo Menjalankan server frontend dan backend di terminal baru...
 wt -d . cmd /k "php artisan serve" ; new-tab -d . cmd /k "npm run dev"
