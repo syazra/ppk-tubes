2178737934762b2ad6869daa2b5136a4e17ef465
ruthhhs <ruthseptrianas@gmail.com>
2026-09-25 14:12:28 +0700

    mod: profile sttings for user

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -2,15 +2,15 @@
     <!-- Left Sidebar -->
     <x-slot name="sidebar">
         @if(auth()->check() && auth()->user()->role == 'admin')
-            <!-- Jika role user adalah admin -->
             @include('admin.navbar')
             
         @elseif(auth()->check() && auth()->user()->role == 'user')
-            <!-- Jika role user adalah user biasa -->
             @include('user.navbar')
             
+        @elseif(auth()->check() && auth()->user()->role == 'operator')
+            @include('operator.navbar')
+
         @else
-            <!-- Jika belum login atau role tidak dikenali (opsional) -->
             @include('default.navbar')
         @endif
     </x-slot>
@@ -33,9 +33,9 @@
         </div>
     </x-white-card>
 
-    <x-white-card>
+    <!-- <x-white-card>
         <div class="max-w-xl">
             @include('profile.partials.delete-user-form')
         </div>
-    </x-white-card>
+    </x-white-card> -->
 </x-app-layout>
